ff1f5bb9d85097e5ff2d5c103643c53da3a4f89e
Manuel Gil <manuelgilsitio@gmail.com>
2022-08-27 17:56:02 -0500

    update

--- mlflow_tracking.py
+++ mlflow_tracking.py
@@ -6,4 +6,8 @@ if __name__ == "__main__":
     # Log an artifact (output file)
     with open("outputs/test.txt", "w") as f:
         f.write("hello world!")
+    with open("outputs/test1.txt", "w") as f:
+        f.write("hello world from test 1!")
+    with open("outputs/test2.txt", "w") as f:
+        f.write("hello world from test 2!")                
     mlflow.log_artifacts("outputs")
