72fe67a977703ef1e6c0083a9422a0ce86b14913
Manuel Gil <manuelgilsitio@gmail.com>
2022-08-27 14:18:54 -0500

    logging metrics

--- mlflow_tracking.py
+++ mlflow_tracking.py
@@ -4,4 +4,4 @@ import mlflow
 
 if __name__ == "__main__":
     # Log a parameter (key-value pair)
-    mlflow.log_param("param1", randint(0, 100))
+    mlflow.log_metric("metric_1", randint(0, 100))
