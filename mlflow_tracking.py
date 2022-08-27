34f56bc8e38a0ebb55ebb1c431f03f764b0ea97c
Manuel Gil <manuelgilsitio@gmail.com>
2022-08-27 14:12:36 -0500

    logging parameters using a dictionary

--- mlflow_tracking.py
+++ mlflow_tracking.py
@@ -4,7 +4,9 @@ import mlflow
 
 if __name__ == "__main__":
     # Log a parameter (key-value pair)
-    mlflow.log_param("param1", randint(0, 100))
-    mlflow.log_param("param2", randint(0, 100))
-    mlflow.log_param("param3", randint(0, 100))
-    mlflow.log_param("param4", randint(0, 100))
+    mlflow.log_params({
+        "param1": randint(0, 100),
+        "param2": randint(0, 100),
+        "param3": randint(0, 100),
+        "param4": randint(0, 100),
+    })
