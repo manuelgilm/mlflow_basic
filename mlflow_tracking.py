e0a8dc452ef0c887b56b7d9b180d8e43fd4c32bc
Manuel Gil <manuelgilsitio@gmail.com>
2022-08-27 17:20:32 -0500

    update

--- mlflow_tracking.py
+++ mlflow_tracking.py
@@ -3,7 +3,7 @@ from random import random, randint
 import mlflow
 
 if __name__ == "__main__":
-    # Log a parameter (key-value pair)
+    # Log different metrics (key-value pair)
     mlflow.log_metric("metric_1", randint(0, 100))
     mlflow.log_metric("metric_2", randint(0, 100))
     mlflow.log_metric("metric_3", randint(0, 100))
