f60fc8ff233fd8aa8bb367358ac753b299f57d02
Manuel Gil <manuelgilsitio@gmail.com>
2022-08-27 17:18:12 -0500

    logging metrics using dictionary

--- mlflow_tracking.py
+++ mlflow_tracking.py
@@ -4,7 +4,10 @@ import mlflow
 
 if __name__ == "__main__":
     # Log a parameter (key-value pair)
-    mlflow.log_metric("metric_1", randint(0, 100))
-    mlflow.log_metric("metric_2", randint(0, 100))
-    mlflow.log_metric("metric_3", randint(0, 100))
-    mlflow.log_metric("metric_4", randint(0, 100))
+    mlflow.log_metrics({
+        "metric_1":randint(0, 100),
+        "metric_2":randint(0, 100),
+        "metric_3":randint(0, 100),
+        "metric_4":randint(0, 100),
+    })
+    
