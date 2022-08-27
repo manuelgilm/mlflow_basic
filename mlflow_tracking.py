cb875ef9b92190abdceca55a521b3ef56118fbe1
Manuel Gil <manuelgilsitio@gmail.com>
2022-08-27 14:11:19 -0500

    logging several parameters individually

--- mlflow_tracking.py
+++ mlflow_tracking.py
@@ -5,3 +5,6 @@ import mlflow
 if __name__ == "__main__":
     # Log a parameter (key-value pair)
     mlflow.log_param("param1", randint(0, 100))
+    mlflow.log_param("param2", randint(0, 100))
+    mlflow.log_param("param3", randint(0, 100))
+    mlflow.log_param("param4", randint(0, 100))
