8c5a4b4034f3a9dd0ba17ad1aa982bd0af5a712f
Manuel Gil <manuelgilsitio@gmail.com>
2022-08-27 17:14:26 -0500

    using mlflow to log metrics

--- mlflow_tracking.py
+++ mlflow_tracking.py
@@ -5,3 +5,6 @@ import mlflow
 if __name__ == "__main__":
     # Log a parameter (key-value pair)
     mlflow.log_metric("metric_1", randint(0, 100))
+    mlflow.log_metric("metric_2", randint(0, 100))
+    mlflow.log_metric("metric_3", randint(0, 100))
+    mlflow.log_metric("metric_4", randint(0, 100))
