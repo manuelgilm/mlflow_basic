fc22fad2a055cd208acdfd508426c14eba3ef55b
Manuel Gil <manuelgilsitio@gmail.com>
2022-08-28 17:29:49 -0500

    using context manager

--- mlflow_tracking.py
+++ mlflow_tracking.py
@@ -9,11 +9,9 @@ if __name__ == "__main__":
     experiment = client.get_experiment_by_name(experiment_name)
     #get experiment id from experiment
     experiment_id = experiment.experiment_id
-    mlflow.start_run(experiment_id=experiment_id)
+    with mlflow.start_run(experiment_id=experiment_id) as run:
 
-    mlflow.log_param("param1",randint(0,100))
-    mlflow.log_param("param2",randint(0,100))
-    mlflow.log_param("param3",randint(0,100))
-    mlflow.log_param("param4",randint(0,100))
-    
-    mlflow.end_run()
+        mlflow.log_param("param1",randint(0,100))
+        mlflow.log_param("param2",randint(0,100))
+        mlflow.log_param("param3",randint(0,100))
+        mlflow.log_param("param4",randint(0,100))
