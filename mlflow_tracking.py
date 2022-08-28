7bae38cdedb1cd5764bd94f136e5aefbc4baa101
Manuel Gil <manuelgilsitio@gmail.com>
2022-08-28 16:29:46 -0500

    creating mlflow experiment

--- mlflow_tracking.py
+++ mlflow_tracking.py
@@ -1,19 +1,7 @@
-import os
-from random import random, randint
-from mlflow import log_metric, log_param, log_artifacts
+from mlflow.tracking import MlflowClient
 
 if __name__ == "__main__":
-    # Log a parameter (key-value pair)
-    log_param("param1", randint(0, 100))
+    client = MlflowClient()
+    experiment_name = "experiment_2"
+    client.create_experiment(name=experiment_name)
 
-    # Log a metric; metrics can be updated throughout the run
-    log_metric("foo", random())
-    log_metric("foo", random() + 1)
-    log_metric("foo", random() + 2)
-
-    # Log an artifact (output file)
-    if not os.path.exists("outputs"):
-        os.makedirs("outputs")
-    with open("outputs/test.txt", "w") as f:
-        f.write("hello world!")
-    log_artifacts("outputs")
